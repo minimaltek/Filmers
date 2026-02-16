f13b0d43b4b0131df57190a36693de610d004481
minimaltek <cycle2012@gmail.com>
2026-02-16 14:55:49 +0900

    Initial Commit

---
+++ Cinecam/CinecamApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinecamApp.swift
+//  Cinecam
+//
+//  Created by minimaltek on 2026/02/16.
+//
+
+import SwiftUI
+
+@main
+struct CinecamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
